45042555efebf183de6a9f3b98933563f2c93dac
lrutter <lindsayannerutter@gmail.com>
2014-11-18 20:32:15 -0600

    Added first draft of a documented sbTree.rda dataset, so the warning would not persist in R CMD check()

---
+++ R/phyViz-data.R
@@ -0,0 +1,9 @@
+#' @name sbTree
+#' @title Soy bean data
+#' @description This data set list soy bean information
+#' @docType data
+#' @usage sbTree
+#' @format a \code{RData} instance, 1 row per soy bean variety
+#' @source Collected by multiple labs
+#' @author Lindsay Rutter, 2014-11-18
+NULL
