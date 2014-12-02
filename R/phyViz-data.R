6249eb6ca4e78ae1bc67747c96614e9f11ee93a9
lrutter <lindsayannerutter@gmail.com>
2014-12-02 11:48:07 -0600

    Updated spelling issue

--- R/phyViz-data.R
+++ R/phyViz-data.R
@@ -4,6 +4,6 @@
 #' @docType data
 #' @usage sbTree
 #' @format a \code{RData} instance, 1 row per soy bean variety
-#' @source The available data consists of a data frame structure that contains 412 direct child-parent relationships between pairs of soybean varieties. These data were collected from frield trials, genetic studies, and United States Department of Agriculture (USDA) bulletins, and date as early as the first decade of the 1900s.
+#' @source The available data consists of a data frame structure that contains 412 direct child-parent relationships between pairs of soybean varieties. These data were collected from field trials, genetic studies, and United States Department of Agriculture (USDA) bulletins, and date as early as the first decade of the 1900s.
 #' @author Lindsay Rutter, 2014-11-18
 NULL
