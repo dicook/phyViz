ef894172331abe5ecbe648b172e16b5d9292e6df
lrutter <lindsayannerutter@gmail.com>
2014-12-06 11:19:38 -0600

    Renamed data description file to sbTree.R, and added a new data description file called bTree.R

---
+++ R/bTree.R
@@ -0,0 +1,9 @@
+#' @name bTree
+#' @title Barley data
+#' @description This data set list barley information
+#' @docType data
+#' @usage bTree
+#' @format a \code{RData} instance, 1 row per barley variety
+#' @source The available data consists of a data frame structure that contains 2505 direct child-parent relationships between pairs of barley varieties.
+#' @author Lindsay Rutter, 2014-12-06
+NULL
