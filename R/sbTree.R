2a4731eed7f088610ef22376d3ef5ab3f7075601
lrutter <lindsayannerutter@gmail.com>
2014-12-09 09:22:33 -0600

    Updated sbTree file name and content, using GGally recent commits as a reference

--- R/sbTree.R
+++
@@ -1,9 +0,0 @@
-#' @name sbTree
-#' @title Soy bean data
-#' @description This data set list soy bean information
-#' @docType data
-#' @usage sbTree
-#' @format a \code{RData} instance, 1 row per soy bean variety
-#' @source The available data consists of a data frame structure that contains 412 direct child-parent relationships between pairs of soybean varieties. These data were collected from field trials, genetic studies, and United States Department of Agriculture (USDA) bulletins, and date as early as the first decade of the 1900s.
-#' @author Lindsay Rutter, 2014-11-18
-NULL
